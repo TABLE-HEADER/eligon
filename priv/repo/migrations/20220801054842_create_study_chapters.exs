1b0183b084fd9af380402eb3bfebec4a764a54ae
moehayamamoto <moeha.yamamoto@gmail.com>
2022-08-01 16:41:20 +0900

    Revert "22/08/01PM"
    This reverts commit 1f9826465ec5f1a57f1c18dd6a5a8ef51768af86.

--- priv/repo/migrations/20220801054842_create_study_chapters.exs
+++
@@ -1,9 +0,0 @@
-defmodule Eligon.Repo.Migrations.CreateStudyChapters do
-  use Ecto.Migration
-
-  def change do
-    create table(:study_chapters) do
-      add :title, :string, null: false
-    end
-  end
-end
