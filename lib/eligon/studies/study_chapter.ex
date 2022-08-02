4459fd7c10a6e25f9046647a7a1b6f1dcadefc0e
moehayamamoto <moeha.yamamoto@gmail.com>
2022-08-02 10:21:32 +0900

    table has been created!

---
+++ lib/eligon/studies/study_chapter.ex
@@ -0,0 +1,10 @@
+defmodule Eligon.Studies.StudyChapter do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  #@foreign_key_type :binary_id
+
+  schema "study_chapters" do
+    field(:title, :string)
+  end
+end
