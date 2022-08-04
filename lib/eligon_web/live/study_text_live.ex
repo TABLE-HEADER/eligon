25f5eaab48078a2973cb22e7680f12a2053ef932
OSnonono <inglis_dili_geri_gelib@yahoo.co.jp>
2022-08-04 13:54:17 +0900

    チェックボックスとDBの連携が終わりました。

--- lib/eligon_web/live/study_text_live.ex
+++ lib/eligon_web/live/study_text_live.ex
@@ -12,10 +12,9 @@ defmodule EligonWeb.StudyTextLive do
     EligonWeb.StudyTextView.render(assigns.template, assigns)
   end
 
-  def mount(_params, _seesion, socket) do
-    IO.inspect(Map.from_struct(socket))
+  def mount(_params, session, socket) do
     textbook = Studies.get_textbook()
-    advances = Studies.get_all_advances_by_userid(4)
+    advances = Studies.get_all_advances_by_userid(session["user_id"])
 
     socket =
       socket
@@ -30,13 +29,25 @@ defmodule EligonWeb.StudyTextLive do
 
   def handle_event("check", %{"study_advance" => study_advance}, socket) do
     IO.inspect(study_advance)
+    # こういう型で入ってきています。
+    # %{"is_read" => "true", "lesson_id" => "1", "user_id" => "4"}
+    %{"is_read" => is_read, "lesson_id" => lesson_id, "user_id" => user_id}
+      = study_advance
 
-    if study_advance["is_read"] do
-      #
+    # 再束縛しています！！(元の文字列を適切なものにしました。)
+    is_read = String.to_atom(is_read)
+    lesson_id = String.to_integer(lesson_id)
+    user_id = String.to_integer(user_id)
+
+    if is_read do
+      Studies.get_advance_and_update(user_id, lesson_id, :true)
     else
-      #
+      Studies.get_advance_and_update(user_id, lesson_id, :false)
     end
 
+    IO.inspect("成功！！")
+    IO.inspect("---------------------------")
+    IO.inspect("at study_text_live.ex:48")
     {:noreply, socket}
   end
 end
