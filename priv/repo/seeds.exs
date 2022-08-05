b1974c46cccd854f04ab47a7111fe0faa25db581
OSnonono <OSnonono>
2022-08-05 01:04:02 +0000

    教材説明をseedsに追加しました。レイアウトに注意です。

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -25,22 +25,22 @@ chapters = [
 ]
 
 lessons = [
-  %StudyLesson{chapter_id: 1, index: 1, lesson: "インストール", link: "https://elixir-lang.jp/install.html"},
-  %StudyLesson{chapter_id: 1, index: 2, lesson: "VS Codeのインストール", link: "https://www.javadrive.jp/vscode/install/index1.html"},
+  %StudyLesson{chapter_id: 1, index: 1, lesson: "インストール", description: "Elixir, OTP等をインストールします。", link: "https://elixir-lang.jp/install.html"},
+  %StudyLesson{chapter_id: 1, index: 2, lesson: "VS Codeのインストール", description: "コードエディタであるVS Codeをインストールします。", link: "https://www.javadrive.jp/vscode/install/index1.html"},
 
-  %StudyLesson{chapter_id: 2, index: 1, lesson: "Hello Worldを表示してみよう", link: "https://elixir-lang.jp/getting-started/introduction.html"},
-  %StudyLesson{chapter_id: 2, index: 2, lesson: "基本", link: "https://elixirschool.com/ja/lessons/basics/basics"},
-  %StudyLesson{chapter_id: 2, index: 3, lesson: "コレクション", link: "https://elixirschool.com/ja/lessons/basics/collections"},
-  %StudyLesson{chapter_id: 2, index: 4, lesson: "Enum", link: "https://elixirschool.com/ja/lessons/basics/enum"},
-  %StudyLesson{chapter_id: 2, index: 5, lesson: "パターンマッチング", link: "https://elixirschool.com/ja/lessons/basics/pattern_matching"},
+  %StudyLesson{chapter_id: 2, index: 1, lesson: "Hello Worldを表示してみよう", description: "簡単な文字列を表示できるようにしてみましょう。", link: "https://elixir-lang.jp/getting-started/introduction.html"},
+  %StudyLesson{chapter_id: 2, index: 2, lesson: "基本", description: "Elixirの基本を学びましょう。", link: "https://elixirschool.com/ja/lessons/basics/basics"},
+  %StudyLesson{chapter_id: 2, index: 3, lesson: "コレクション", description: "リスト、タプル、キーワードリスト、マップなど", link: "https://elixirschool.com/ja/lessons/basics/collections"},
+  %StudyLesson{chapter_id: 2, index: 4, lesson: "Enum", description: "Elixirでよく使うEnumモジュールを学びましょう。", link: "https://elixirschool.com/ja/lessons/basics/enum"},
+  %StudyLesson{chapter_id: 2, index: 5, lesson: "パターンマッチング", description: "Elixirの強みであるパターンマッチングを学びます。", link: "https://elixirschool.com/ja/lessons/basics/pattern_matching"},
 
-  %StudyLesson{chapter_id: 3, index: 1, lesson: "OTPの並行性", link: "https://elixirschool.com/ja/lessons/advanced/otp_concurrency"},
-  %StudyLesson{chapter_id: 3, index: 2, lesson: "OTPスーパバイザ", link: "https://elixirschool.com/ja/lessons/advanced/otp_supervisors"},
-  %StudyLesson{chapter_id: 3, index: 3, lesson: "OTPディストリビューション", link: "https://elixirschool.com/ja/lessons/advanced/otp_distribution"},
+  %StudyLesson{chapter_id: 3, index: 1, lesson: "OTPの並行性", description: "Elixirに組み込まれているOTPの振る舞いに目を向けます。", link: "https://elixirschool.com/ja/lessons/advanced/otp_concurrency"},
+  %StudyLesson{chapter_id: 3, index: 2, lesson: "OTPスーパバイザ", description: "スーパバイザは、他のプロセスを監視するという一つの目的に特化したプロセスです。 ", link: "https://elixirschool.com/ja/lessons/advanced/otp_supervisors"},
+  %StudyLesson{chapter_id: 3, index: 3, lesson: "OTPディストリビューション", description: "Elixirは、このレッスンで概説するいくつかの異なるメカニズムを通して、それらのノード間で通信をすることが可能です。", link: "https://elixirschool.com/ja/lessons/advanced/otp_distribution"},
 
-  %StudyLesson{chapter_id: 4, index: 1, lesson: "Ectoの基本", link: "https://elixirschool.com/ja/lessons/ecto/basics"},
-  %StudyLesson{chapter_id: 4, index: 2, lesson: "チェンジセット", link: "https://elixirschool.com/ja/lessons/ecto/changesets"},
-  %StudyLesson{chapter_id: 4, index: 3, lesson: "アソシエーション", link: "https://elixirschool.com/ja/lessons/ecto/associations"}
+  %StudyLesson{chapter_id: 4, index: 1, lesson: "Ectoの基本", description: "Ectoはデータベースラッパーと統合クエリ言語を提供するElixirの公式なプロジェクトです。", link: "https://elixirschool.com/ja/lessons/ecto/basics"},
+  %StudyLesson{chapter_id: 4, index: 2, lesson: "チェンジセット", description: "データの挿入、更新等にチェンジセットが必要になります。", link: "https://elixirschool.com/ja/lessons/ecto/changesets"},
+  %StudyLesson{chapter_id: 4, index: 3, lesson: "アソシエーション", description: "スキーマ間のアソシエーション定義を学びましょう。", link: "https://elixirschool.com/ja/lessons/ecto/associations"}
 ]
 
 Enum.each(chapters, &(Repo.insert(&1)))
